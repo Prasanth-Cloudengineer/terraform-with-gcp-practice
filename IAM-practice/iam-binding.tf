df1bbdd1d2c47daab6b93d9a4457fabfe86cf640
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-07-22 13:25:18 +0530

    role binding

--- IAM-practice/iam-binding.tf
+++ IAM-practice/iam-binding.tf
@@ -1,4 +1,9 @@
 #Assign role to a user
+
+provider "google"{
+
+}
+
 resource "google_project_iam_member" "storage_viewer"{
     project = "prj-github-prasanth"
     role    = "roles/storage.bucketViewer"
